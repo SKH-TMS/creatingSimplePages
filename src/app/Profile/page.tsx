d88b55bb8c2659ed882026b6e1dc86b68b098732
SKH-TMS <k.s.h.taskmanagement@gmail.com>
2024-10-06 09:59:17 +0500

    Add files via upload

---
+++ src/app/Profile/page.tsx
@@ -0,0 +1,10 @@
+export default function Profile() {
+  return (
+    <div>
+      <div className="bg-cyan-500 my-5">
+        <h1 className="text-center text-2xl my-5">Profile Page Data</h1>
+        <p className="text-center text-red-900">User profile</p>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
-
+import Login from "./Login/page";
+import Register from "./Register/page";
+import Profile from "./Profile/page";
 
 export default function Home() {
   return (
     <div>
-      <h2>Landing page</h2>
-    </div>  );
+      <h1 className="text-center text-2xl my-5">NestJS Application</h1>
+      <p className="text-center">
+        I am a web developer and learning web development on base of awesome
+        framework NestJS. This framework is of JavaScript.{" "}
+      </p>
+      {/* below is calling components in this home page */}
+      <Login />
+      <Register />
+      <Profile />
+    </div>
+  );
 }
